5c81565c6203ffc5f9c11130f553a0b9f0d9f735
Yuri Cunha <isyuricunha@duck.com>
2022-12-04 03:23:40 -0300

    put comments

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,15 +2,18 @@ print('********************************')
 print('Welcome to the Guessing Game!')
 print('********************************')
 
-secretNumber = 42
+secretNumber = 42  # The number you need to discover.
 
-shot = input('Put your number: ')
+shot = input('Put your number: ')  # For request a number.
 
-print('You put the number', shot)
+print('You put the number', shot)  # For show the select number.
 
-shot = int(shot)
+shot = int(shot)  # Here we transform the variable `shot` to inter, because before of this, is a str (string). You can
+# see iso with type(shot) in terminal.
 
-if secretNumber == shot:
+if secretNumber == shot:  # A logic to recognize if it is a correct number or not
     print('You won')
 else:
     print('You lose.')
+
+# I hope you can guess the number... Or not.
