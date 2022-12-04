0861b17a390979fb2f7ebfb3fc3fb4c826ec083b
Yuri Cunha <isyuricunha@duck.com>
2022-12-04 03:13:11 -0300

    fix to work

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,16 @@
+print('********************************')
 print('Welcome to the Guessing Game!')
+print('********************************')
+
+secretNumber = 42
+
+shot = input('Put your number: ')
+
+print('You put the number', shot)
+
+shot = int(shot)
+
+if secretNumber == shot:
+    print('You won')
+else:
+    print('You lose.')
